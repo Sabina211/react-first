67c68aa9f38ca9518f85795b7203a0c976d90c16
Sabina <iamsabina21@gmail.com>
2025-02-19 21:02:49 +0500

    добавила кружочек

--- src/components/burger-ingredients/ingredient-item/ingredient-item.js
+++ src/components/burger-ingredients/ingredient-item/ingredient-item.js
@@ -1,38 +1,55 @@
-import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+	CurrencyIcon,
+	Counter,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient-item.module.css';
 import { useState } from 'react';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 
-function IngredientItem({  ingredient }) {
+function IngredientItem({ ingredient }) {
 	const [show, setShow] = useState(false);
 	const count = Math.floor(Math.random() * 2);
-	console.log(count)
+	console.log(count);
 
 	function showDetails() {
-		console.log(ingredient.name)
+		console.log(ingredient.name);
 		setShow(true);
 	}
 
 	function hideDetails() {
-		console.log("hideDetails")
+		console.log('hideDetails');
 		setShow(false);
 	}
 	return (
-		<div>
-		<li className={styles.card} onClick={showDetails}>
-			<img src={ingredient.image} alt={ingredient.name} />
-			<div className={`${styles.cardText}`}>
-				<span className='text text_type_digits-default mr-2'>{ingredient.price}</span>
-				<CurrencyIcon type='primary' />
-			</div>
-			<div className={`${styles.cardText} text text_type_main-default`}>
-				{ingredient.name}
-			</div>
+		<>
+			<li className={styles.card} onClick={showDetails}>
+				<div className={styles.imageWrapper}>
+					<img src={ingredient.image} alt={ingredient.name} />
+					{count && count > 0 ? <Counter
+						count={count}
+						size='default'
+						extraClass={`${styles.count} "m-1"`}
+					/> : undefined}
+				</div>
+				<div className={`${styles.cardText}`}>
+					<span className='text text_type_digits-default mr-2'>
+						{ingredient.price}
+					</span>
+					<CurrencyIcon type='primary' />
+				</div>
+				<div className={`${styles.cardText} text text_type_main-default`}>
+					{ingredient.name}
+				</div>
+			</li>
 
-		</li>
-		<Counter count={count} size="default" extraClass={`${styles.count} "m-1"` } />
-		{show && <IngredientDetails isOpen={show} onClose={hideDetails} ingredient={ingredient}/>}
-		</div>
+			{show && (
+				<IngredientDetails
+					isOpen={show}
+					onClose={hideDetails}
+					ingredient={ingredient}
+				/>
+			)}
+		</>
 	);
 }
 
